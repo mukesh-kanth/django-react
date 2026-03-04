7230ec35acfa8471c333673d6df99d1a72158abb
mukesh-kanth <varunharish0786@gmail.com>
2026-03-04 09:39:09 +0530

    Ready for deployment

--- sample/settings.py
+++ sample/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^jx#iee0!$d*-m=ocd!t%uuspza-!84d#e^2$%j&oyl#)d5q1w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
